2bbe3fa7139fd822176b4167ad57a9ae8b4ab0da
Edi Modrić <emodric@users.noreply.github.com>
2016-09-28 01:52:43 +0200

    Remove processing config from DI extension

--- DependencyInjection/NetgenSiteAccessRoutesExtension.php
+++ DependencyInjection/NetgenSiteAccessRoutesExtension.php
@@ -14,9 +14,6 @@ class NetgenSiteAccessRoutesExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
-
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
